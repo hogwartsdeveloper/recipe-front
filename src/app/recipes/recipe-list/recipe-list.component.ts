027211442988afca33e35ada9021648b7e500cea
zhannur <jannuraidynuly@gmail.com>
2023-06-07 08:30:34 +0600

    recipe detail bind data

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {Recipe} from "../recipe.model";
 
 @Component({
@@ -9,15 +9,21 @@ import {Recipe} from "../recipe.model";
 export class RecipeListComponent {
   recipes: Recipe[] = [
     new Recipe(
-        'A Test Recipe',
+        'A Test Recipe 1',
         'This is simply a test', '' +
         'https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe-500x500.jpg'
     ),
     new Recipe(
-        'A Test Recipe',
+        'A Test Recipe 2',
         'This is simply a test', '' +
         'https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe-500x500.jpg'
     )
   ];
+
+  @Output() selectedRecipe = new EventEmitter<Recipe>();
+
+  onSelectRecipe(recipe: Recipe) {
+    this.selectedRecipe.emit(recipe);
+  }
 
 }
