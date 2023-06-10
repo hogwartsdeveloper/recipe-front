fcaf5250e82fa4e21c0345293445e66ca33b26e8
zhannur <jannuraidynuly@gmail.com>
2023-06-10 19:52:42 +0600

    shopping list service

---
+++ src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,31 @@
+import {Ingredient} from "../shared/ingredient.model";
+import {EventEmitter} from "@angular/core";
+
+export class ShoppingListService {
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10)
+    ];
+
+    changeIngredient = new EventEmitter<Ingredient[]>();
+
+    getIngredient() {
+        return this.ingredients.slice();
+    }
+
+    onAdd(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+        this.changeIngredient.emit(this.getIngredient());
+    }
+
+    onDelete(ingredient: Ingredient) {
+        this.ingredients = this.ingredients.filter(
+            el => el.name !== ingredient.name && el.amount !== ingredient.amount);
+        this.changeIngredient.emit(this.getIngredient());
+    }
+
+    onClear() {
+        this.ingredients = [];
+        this.changeIngredient.emit(this.getIngredient());
+    }
+}
