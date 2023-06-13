90cf5af3436b5eb10d49a0698ff3fc5003223aa1
zhannur <jannuraidynuly@gmail.com>
2023-06-13 07:41:46 +0600

    recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,17 +1,26 @@
 import {Recipe} from "./recipe.model";
 import {EventEmitter} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService {
     private recipes: Recipe[] = [
         new Recipe(
             'A Test Recipe 1',
             'This is simply a test', '' +
-            'https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe-500x500.jpg'
+            'https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe-500x500.jpg',
+            [
+                new Ingredient('Meat', 2),
+                new Ingredient('Kinza', 10)
+            ]
         ),
         new Recipe(
             'A Test Recipe 2',
             'This is simply a test', '' +
-            'https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe-500x500.jpg'
+            'https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe-500x500.jpg',
+            [
+                new Ingredient('Bread', 10),
+                new Ingredient('Banana', 5)
+            ]
         )
     ];
 
