8830abadef6969c011c600f2ac6940cd60bd88bd
zhannur <jannuraidynuly@gmail.com>
2023-07-13 08:53:04 +0600

    create user model

---
+++ src/app/auth/models/user.model.ts
@@ -0,0 +1,14 @@
+export class User {
+    constructor(
+        public email: string,
+        private _token: string,
+        private _tokenExpirationDate: number
+    ) {}
+
+    get token() {
+        if (!this._tokenExpirationDate || new Date().getTime() > this._tokenExpirationDate) {
+            return null;
+        }
+        return this._token;
+    }
+}
