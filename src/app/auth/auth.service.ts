1ba3cb766e8f8694132dcf34b7d8ddf8340d76c4
zhannur <jannuraidynuly@gmail.com>
2023-07-16 13:28:19 +0600

    fix

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -105,7 +105,7 @@ export class AuthService {
                     return of(null);
                 }))
                 .subscribe();
-        }, tokenExpiration - 1000 * 5);
+        }, tokenExpiration - 60000);
     }
 
     private clearLogoutTimer() {
